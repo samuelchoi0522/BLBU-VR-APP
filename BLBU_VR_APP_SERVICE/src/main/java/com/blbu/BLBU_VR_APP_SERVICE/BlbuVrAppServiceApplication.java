981c04b25c63be3c209d01bba40ee0ecc3cd28f9
samuelchoi0522 <jaedolc@gmail.com>
2025-09-24 15:15:34 -0500

    refactor CI/CD pipeline configuration and add initial project files

---
+++ BLBU_VR_APP_SERVICE/src/main/java/com/blbu/BLBU_VR_APP_SERVICE/BlbuVrAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blbu.BLBU_VR_APP_SERVICE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlbuVrAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlbuVrAppServiceApplication.class, args);
+	}
+
+}
---
+++ BLBU_VR_APP_SERVICE/src/test/java/com/blbu/BLBU_VR_APP_SERVICE/BlbuVrAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blbu.BLBU_VR_APP_SERVICE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlbuVrAppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
